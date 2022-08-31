21c14cd0a707c29ca7c64c03cb447df3afdd7548
Zifeng2022 <112544389+Zifeng2022@users.noreply.github.com>
2022-08-31 21:26:05 +0800

    Bull&Bear
    a cow

---
+++ link/code-sample/contract-76a7350823.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.7.3/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("MyToken", "MTK") {}
+}
